375fa6a73c17f75a95f5cd5e95d1023c5029a561
siddharth00911 <siddharth00911@gmail.com>
2022-12-31 19:13:39 +0530

    c

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
       <h2>this is simple react app</h2>
 
       <strong>WEB APP</strong>
+
+      <body>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quis eos eum, repudiandae, cum, aliquid iusto corrupti
+          nostrum maiores asperiores quisquam quos. Dignissimos blanditiis iusto maiores modi consequuntur voluptatibus nam,
+          quod magnam distinctio rem id praesentium, alias accusantium ea et repellendus, reprehenderit nobis. Illo,
+          asperiores provident eum officia sapiente impedit quam culpa aliquam nobis, omnis distinctio, hic esse possimus!
+          Vero explicabo repudiandae iusto, at et repellat. Neque architecto illo nulla fuga quia, natus facere nobis id vitae
+          beatae numquam porro, laudantium, facilis et aliquid odio quod iure reprehenderit? Esse id optio impedit qui nisi
+          totam doloremque est officiis voluptate numquam.</p>
+      </body>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
