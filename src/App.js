07d2b34e1c191addfc7094c8ab7e11e2c5adc55b
siddharth00911 <siddharth00911@gmail.com>
2023-01-01 23:20:34 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
 // import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <h2>this is simple react app</h2>
+    return (
+        <div className="App">
 
-      <strong>WEB APP</strong>
 
-      <body>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quis eos eum, repudiandae, cum, aliquid iusto corrupti
-          nostrum maiores asperiores quisquam quos. Dignissimos blanditiis iusto maiores modi consequuntur voluptatibus nam,
-          quod magnam distinctio rem id praesentium, alias accusantium ea et repellendus, reprehenderit nobis. Illo,
-          asperiores provident eum officia sapiente impedit quam culpa aliquam nobis, omnis distinctio, hic esse possimus!
-          Vero explicabo repudiandae iusto, at et repellat. Neque architecto illo nulla fuga quia, natus facere nobis id vitae
-          beatae numquam porro, laudantium, facilis et aliquid odio quod iure reprehenderit? Esse id optio impedit qui nisi
-          totam doloremque est officiis voluptate numquam.</p>
-      </body>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+            <body>
+                <h2>SIMPLE REACT APP</h2>
+
+
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, consectetur.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quis eos eum, repudiandae, cum, aliquid iusto corrupti
+                    nostrum maiores asperiores quisquam quos. Dignissimos blanditiis iusto maiores modi consequuntur voluptatibus nam,
+                    quod magnam distinctio rem id praesentium, alias accusantium ea et repellendus, reprehenderit nobis. Illo,
+                    asperiores provident eum officia sapiente impedit quam culpa aliquam nobis, omnis distinctio, hic esse possimus!
+                    Vero explicabo repudiandae iusto, at et repellat. Neque architecto illo nulla fuga quia, natus facere nobis id vitae
+                    beatae numquam porro, laudantium, facilis et aliquid odio quod iure reprehenderit? Esse id optio impedit qui nisi
+                    totam doloremque est officiis voluptate numquam.</p>
+
+
+
+            </body>
+
+        </div>
+    );
 }
 
 export default App;
+
+
+
+// // new
+
+// import { createElement as ce } from 'react';
+// import { ReactDOM } from 'react-dom/client';
+
+// {/* JSX */ }
+
+// const heading = <h1 id="title1" key="h11"> NAMASTE React</h1>
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// export default App;
